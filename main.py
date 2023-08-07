63bf7c1317c5e307545191f3fdcbc7917aaf0253
Parag <paraglondhe123@gmail.com>
2023-08-07 22:54:07 +0530

    added more data

--- main.py
+++ main.py
@@ -12,14 +12,13 @@ def preprocess(impath):
         return image
     except cv.error:
         print("Invalid Image !")
-        image = np.zeros(196,196,3)
+        image = np.zeros((196, 196 ,3),dtype='uint8')
         return image
 def predict(impath,model):
     imarray = []
     imarray.append(preprocess(impath))
     x = np.array(imarray)
     return np.round(model.predict(x),3)
-
 def predictstr(impath,model):
     prediction = predict(impath,model)
     result = prediction[0][0]
@@ -34,10 +33,15 @@ if __name__ == '__main__':
     model = tf.keras.models.load_model(modelpath)
     PATH = 'Test_images/'
     imglist = os.listdir(PATH)
-    imname = imglist[2]
+    imname = imglist[3]  #<-----(Enter image name here to predict)---------
     impath = os.path.join(PATH,imname)
     img = cv.imread(impath)
-    cv.putText(img,predictstr(impath,model),(20,30),cv.FONT_HERSHEY_SIMPLEX,1.0,(0,255,0),thickness=2)
-    cv.imshow('Result',img)
+    try :
+        cv.putText(img, predictstr(impath, model), (20, 30), cv.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0), thickness=2)
+        cv.imshow('Result', img)
+    except cv.error :
+        img = np.zeros((500, 500, 3), dtype='uint8')
+        cv.putText(img, "INVALID IMAGE", (125, 250), cv.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0), thickness=2)
+        cv.imshow('Result', img)
     cv.waitKey(0)
     # print(predictstr(impath,model))
